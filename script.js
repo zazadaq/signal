1947bdda9511be0a2918a5a22aa43ad64a7b3adf
dadadada <150011820+zazadaq@users.noreply.github.com>
2024-04-14 19:31:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
     const randomImageElement = document.getElementById("randomImage");
     const getRandomImageButton = document.getElementById("getRandomImage");
+    const overlay = document.getElementById("overlay");
 
-    // Список с именами файлов изображений
     const imageNames = [
         "image1.jpg",
         "image2.jpg",
         "image3.jpg"
     ];
 
-    // Функция для получения случайного имени файла изображения
     function getRandomImageName() {
         const randomIndex = Math.floor(Math.random() * imageNames.length);
         return imageNames[randomIndex];
     }
 
-    // Функция для формирования URL-адреса случайного изображения
     function getRandomImageURL() {
         const imageName = getRandomImageName();
-        return `image/${imageName}`;
+        return `images/${imageName}`;
     }
 
-    // Функция для получения случайной картинки
     function getRandomImage() {
-        const randomImageURL = getRandomImageURL();
-        randomImageElement.src = randomImageURL;
+        // Показываем серый фон
+        overlay.style.display = "block";
+
+        // Запускаем таймер на 3 секунды
+        setTimeout(function() {
+            const randomImageURL = getRandomImageURL();
+            randomImageElement.src = randomImageURL;
+
+            // Скрываем серый фон после смены изображения
+            overlay.style.display = "none";
+        }, 3000);
     }
 
-    // Получение случайной картинки при нажатии на кнопку
-    getRandomImageButton.addEventListener("click", getRandomImage);
-
-    // Первичное получение случайной картинки при загрузке страницы
-    getRandomImage();
+    getRandomImageButton.addEventListener("click", function() {
+        getRandomImage();
+    });
 });
